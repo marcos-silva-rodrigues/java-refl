9b6c2cb5c1b56bdc9b43d33d37b568885fdce33d
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-26 16:27:53 -0300

    feat: base classes

---
+++ src/main/java/com/rodrigues/silva/marcos/PessoaDTO.java
@@ -0,0 +1,12 @@
+package com.rodrigues.silva.marcos;
+
+public class PessoaDTO {
+
+  public final String nome;
+  public final String cpf;
+
+  public PessoaDTO( String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+}
---
+++ src/main/java/com/rodrigues/silva/marcos/PessoaRepository.java
@@ -0,0 +1,8 @@
+package com.rodrigues.silva.marcos;
+
+public class PessoaRepository {
+
+  public Pessoa list() {
+    return new Pessoa(1, "João", "1234");
+  }
+}
